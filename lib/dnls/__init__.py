8893f25cbff348fab36dc628f9010526754e60ef
gauenk <kent.gauen@gmail.com>
2022-09-08 12:45:19 -0400

    added pytorch subdir

--- lib/dnls/__init__.py
+++ lib/dnls/__init__.py
@@ -1,26 +1,2 @@
-
-
-# -- submodules --
-from . import search
-from . import reducers
-from . import tile
-from . import tile_k
-from . import simple
-from . import testing
-from . import utils
-
-#
-# -- unpack functions into namespace --
-#
-
-# -- tiling --
-from .tile.fold import fold,Fold
-from .tile.unfold import unfold,Unfold
-from .tile.ifold import ifold,iFold
-from .tile.ifoldz import ifoldz,iFoldz
-from .tile.iunfold import iunfold,iUnfold
-
-# -- tiling k --
-from .tile_k.fold_k import fold_k,FoldK
-from .tile_k.unfold_k import unfold_k,UnfoldK
-
+from . import pytorch
+from .pytorch import * # backward compat.
