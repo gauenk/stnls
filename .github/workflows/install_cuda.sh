7c5cd5cc4839f32be4ebe53664bbe6d1d5f1b751
gauenk <kent.gauen@gmail.com>
2022-09-09 15:44:49 -0400

    update cuda install

--- .github/workflows/install_cuda.sh
+++ .github/workflows/install_cuda.sh
@@ -2,8 +2,10 @@
 export INSTALLER=cuda-11-3_11.3.0-1_amd64.deb
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/${INSTALLER}
 sudo dpkg -i ${INSTALLER}
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
-sudo apt-key add cuda-ubuntu2004.pin
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-keyring_1.0-1_all.deb
+# https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1604/x86_64/3bf863cc.pub
+#wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-ubuntu2004.pin
+sudo apt-key add cuda-keyring_1.0-1_all.deb
 sudo apt update -qq
 sudo apt install -y cuda-11-3 cuda-cufft-dev-11-3
 sudo apt clean
