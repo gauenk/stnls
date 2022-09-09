1c2ddb9604b6138c316a05d5f2f2c378ce66790a
gauenk <kent.gauen@gmail.com>
2022-09-09 15:52:21 -0400

    update cuda install

--- .github/workflows/install_cuda.sh
+++ .github/workflows/install_cuda.sh
@@ -13,12 +13,12 @@
 # export LD_LIBRARY_PATH=${CUDA_HOME}/lib64:${CUDA_HOME}/include:${LD_LIBRARY_PATH}
 # export PATH=${CUDA_HOME}/bin:${PATH}
 
-rm /etc/apt/sources.list.d/cuda.list && \
-rm /etc/apt/sources.list.d/nvidia-ml.list && \
-apt-key del 7fa2af80 && \
-apt-get update && apt-get install -y --no-install-recommends wget && \
-wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-keyring_1.0-1_all.deb && \
-dpkg -i cuda-keyring_1.0-1_all.deb && \
+rm /etc/apt/sources.list.d/cuda.list
+rm /etc/apt/sources.list.d/nvidia-ml.list
+apt-key del 7fa2af80
+apt-get update && apt-get install -y --no-install-recommends wget
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/cuda-keyring_1.0-1_all.deb
+dpkg -i cuda-keyring_1.0-1_all.deb
 apt-get update
 export INSTALLER=cuda-11-3_11.3.0-1_amd64.deb
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu2004/x86_64/${INSTALLER}
